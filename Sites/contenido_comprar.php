b297c2af845f94900304244db2bd78707ddc379e
Javiera Larraín <javipaz.larrain@uc.cl>
2023-11-18 19:48:12 -0300

    ultimos cambios

--- Sites/contenido_comprar.php
+++ Sites/contenido_comprar.php
@@ -21,9 +21,19 @@
         <br>
         
         <!-- -->
-        <p style="text-align:center;">
-          Dropdown con provedor para comprar
-        </p>
+        <div class='container' style="width: 70%">
+          <h3>Seleciona un proveedor para comprar</h3>
+          <br>
+          <form action='./queries/prodecimiento02.php' method='GET'>
+            <select class="form-select form-select-lg mb-3" aria-label="Large select example">
+              <option selected>Selecciona un Proveedor</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+            <button type="submit" class="btn btn-primary" onclick="window.location.href='/Sites/historial_compras.php'">Submit</button>
+          </form>
+        </div>
 
 
     </div>
