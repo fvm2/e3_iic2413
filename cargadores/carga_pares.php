368a1f41151bdb439090b69b3df8aa52f14c7d67
Felipe Valderrama <felipevalderrama@MacBook-Air-de-Felipe-2.local>
2023-11-19 16:52:18 -0300

    act

--- cargadores/carga_pares.php
+++ cargadores/carga_pares.php
@@ -1,87 +1,41 @@
 <?php
-    // Crear conexión a la base de datos
-  $db_host = 'localhost';
-  $db_user = 'grupo98';
-  $db_password = 'magnesio100';
-  $db_name = 'grupo98e3';
+// Crear conexión a la base de datos
+$db_host = 'localhost';
+$db_user = 'grupo98';
+$db_password = 'magnesio100';
+$db_name = 'grupo98e3';
 
-  try {
-      $db = new PDO("pgsql:dbname=$db_name;host=$db_host;port=5432;user=$db_user;password=$db_password");
-      // Configurar el modo de error para lanzar excepciones en caso de error
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch (PDOException $e) {
-      // En caso de error en la conexión, muestra un mensaje y termina el script
-      echo 'Error de conexión: ' . $e->getMessage();
-      exit();
-  }
+try {
+    $db = new PDO("pgsql:dbname=$db_name;host=$db_host;port=5432;user=$db_user;password=$db_password");
+    // Configurar el modo de error para lanzar excepciones en caso de error
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch (PDOException $e) {
+    // En caso de error en la conexión, muestra un mensaje y termina el script
+    echo 'Error de conexión: ' . $e->getMessage();
+    exit();
+}
 ?>
 
-<h1>
-    HTML table code for displaying details like name, rollno, city
-    in tabular format and store in database
-</h1>
 
-<table align="center" width="800"
-    border="1" style=
-    "border-collapse: collapse;  
-    border:1px solid #ddd;"  
-    cellpadding="5"
-    cellspacing="0">
+<?php
+// Get csv file
+if (($handle = fopen("archivo.csv", "r")) !== FALSE) {
+    $n = 1;
+    $stmt = $db->prepare('INSERT INTO tabla() VALUES ()');
 
-    <thead>
-        <tr bgcolor="#FFCC00">
-            <th>
-                <center>ID</center>
-            </th>
-            <th>
-                <center>NOMBRE</center>
-            </th>
-            <th>
-                <center>MAIL</center>
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        // Get csv file
-        if (($handle = fopen("usuarios.csv", "r")) !== FALSE) {
-          $n = 1;
-          $stmt = $db->prepare('INSERT INTO usuarios (id_usuario, nombre, mail, password, username, fecha_nacimiento) VALUES (?, ?, ?, ?, ?, ?)');
-
-          while (($row = fgetcsv($handle)) !== FALSE) {
-              // Skip the header row
-              if ($n > 1) {
-                  if (!$stmt->execute($row)) {
-                      echo "Error al insertar en la fila $n";
-                  }
-        ?>
-                    <tr>
-                        <td>
-                            <center>
-                                <?php echo $row[0]; ?>
-                            </center>
-                        </td>
-                        <td>
-                            <center>
-                                <?php echo $row[1]; ?>
-                            </center>
-                        </td>
-                        <td>
-                            <center>
-                                <?php echo $row[2]; ?>
-                            </center>
-                        </td>
-                    </tr>
-        <?php
-                }
-
-                // Increment record count
-                $n++;
+    while (($row = fgetcsv($handle)) !== FALSE) {
+        // Skip the header row
+        if ($n > 1) {
+            if (!$stmt->execute($row)) {
+                echo "Error al insertar en la fila $n";
             }
+?>
 
-            // Closing the file
-            fclose($handle);
-        }
-        ?>
-    </tbody>
-</table>
+<?php
+// Increment record count
+$n++;
+// Closing the file
+fclose($handle);
+?>
