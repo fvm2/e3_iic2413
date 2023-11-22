3730a0fd144c672c2fffef85ea2a7e2367e9932e
Felipe Valderrama <felipevalderrama@MacBook-Air-de-Felipe-2.local>
2023-11-21 22:54:41 -0300

    errores ultimos cargadores

--- cargadores/carga_horas_juego.php
+++ cargadores/carga_horas_juego.php
@@ -16,13 +16,13 @@ $logFile = 'error_log.txt';
 
 if (($handle = fopen("data/pares/filtered_data/horas_juego.csv", "r")) !== FALSE) {
     $db->beginTransaction();
+    $n = 1;
     $stmt = $db->prepare('INSERT INTO horas_juego (id_usuario,id_videojuego,cantidad,fecha_visualizacion) VALUES (?,?,?,?)');
 
     while (($row = fgetcsv($handle)) !== FALSE) {
-        $n++;
         if ($n > 1) {
-            $id_usuario = $row[1]; 
-            $id_videojuego = $row[2];
+            $id_usuario = $row[0]; 
+            $id_videojuego = $row[1];
 
             if (!llaveprimaria($id_usuario, 'id_usuario', $db, 'usuarios') || !llaveprimaria($id_videojuego,'id_videojuego',$db, 'videojuegos')) {
                 $errorMessage = "Clave foránea no existe para la fila $n";
@@ -32,15 +32,17 @@ if (($handle = fopen("data/pares/filtered_data/horas_juego.csv", "r")) !== FALSE
 
             try {
                 // Asegúrate de que el orden de los elementos en $row coincida con tu consulta SQL
-                $stmt->execute([$row[1], $row[2], $row[3], $row[4]]); 
+                $stmt->execute([$row[0], $row[1], $row[2], $row[3]]); 
             } catch (PDOException $ex) {
                 $db->rollBack();
                 $errorMessage = "Error en la fila $n: " . $ex->getMessage();
                 error_log($errorMessage, 3, $logFile);
+                $n++;
                 fclose($handle);
                 exit('Error al insertar datos. Ver log para detalles.');
             }
         }
+        $n++;
     }
 
     $db->commit();
--- cargadores/carga_opinion_pares.php
+++ cargadores/carga_opinion_pares.php
@@ -17,23 +17,25 @@ $logFile = 'error_log.txt';
 
 if (($handle = fopen("data/pares/filtered_data/opinion.csv", "r")) !== FALSE) {
     $db->beginTransaction();
+    $n = 1;
     $stmt = $db->prepare('INSERT INTO opinion (id_resena,id_usuario,id_videojuego) VALUES (?,?,?)');
 
     while (($row = fgetcsv($handle)) !== FALSE) {
-        $n++;
         if ($n > 1) {
+            $id_resena = $row[0];
             $id_usuario = $row[1]; 
             $id_videojuego = $row[2];
 
             if (!llaveforanea($id_resena, 'id_resena', $db, 'resenas') || !llaveforanea($id_usuario,'id_usuario',$db, 'usuarios') || !llaveforanea($id_videojuego,'id_videojuego',$db, 'videojuegos')) {
                 $errorMessage = "Clave foránea no existe para la fila $n";
                 error_log($errorMessage, 3, $logFile);
+                $n++;
                 continue;
             }
 
             try {
                 // Asegúrate de que el orden de los elementos en $row coincida con tu consulta SQL
-                $stmt->execute([$row[1], $row[2], $row[3], $row[4]]); 
+                $stmt->execute([$row[0], $row[1], $row[2]]); 
             } catch (PDOException $ex) {
                 $db->rollBack();
                 $errorMessage = "Error en la fila $n: " . $ex->getMessage();
@@ -42,6 +44,7 @@ if (($handle = fopen("data/pares/filtered_data/opinion.csv", "r")) !== FALSE) {
                 exit('Error al insertar datos. Ver log para detalles.');
             }
         }
+        $n++;
     }
 
     $db->commit();
